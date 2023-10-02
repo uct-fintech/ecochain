ea61a7862330c2e567688bf7e0730fa99228c9a7
Henri du Plessis <Henri.duPlessis@psg.co.za>
2023-10-02 16:51:19 +0200

    Add flask-login, endpoints for register, login

--- app.py
+++ app.py
@@ -1,13 +1,69 @@
-from flask import Flask
-import sqlite3
+from flask import Flask, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+CREATE_TABLES = False
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-conn = sqlite3.connect('database.db')
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable = False, unique = True)
+    password = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def home():
+    return "Home"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("home"))
+        
+        flash("Invalid username or password")
+    
+    return "Login"
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        
+        hashed_password = generate_password_hash(password)
+        
+        new_user = User(username=username, password=hashed_password, email=email)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for("login"))
+    
+    return "Register"
 
 if __name__ == "__main__":
+    if CREATE_TABLES:
+        with app.app_context():
+            db.create_all()  # This line creates the initial database tables
     app.run(debug=True)
