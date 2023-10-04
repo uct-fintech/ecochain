816b62e6ac0a8d4a5b3f795b8605fb4d71219833
Henri du Plessis <Henri.duPlessis@psg.co.za>
2023-10-04 14:28:34 +0200

    Fixed bug w/ table relationships

--- models.py
+++ models.py
@@ -16,20 +16,21 @@ class Company(db.Model):
     CompanySize = db.Column(db.String(50))
     AlgorandAddress = db.Column(db.String(100))
 
-class PeopleMetrics(db.Model):
+
+class Peoplemetrics(db.Model):
     PeopleMetricsID = db.Column(db.Integer, primary_key=True)
     DiversityAndInclusion = db.Column(db.Double)
     PayEquality = db.Column(db.Double)
     WageLevel = db.Column(db.Double)
     HealthAndSafetyLevel = db.Column(db.Double)
 
-class PlanetMetrics(db.Model):
+class Planetmetrics(db.Model):
     PlanetMetricsID = db.Column(db.Integer, primary_key=True)
     GreenhouseGasEmission = db.Column(db.Double)
     WaterConsumption = db.Column(db.Double)
     LandUse = db.Column(db.Double)
 
-class ProsperityMetrics(db.Model):
+class Prosperitymetrics(db.Model):
     ProsperityMetricsID = db.Column(db.Integer, primary_key=True)
     TotalTaxPaid = db.Column(db.Double)
     AbsNumberOfNewEmps = db.Column(db.Double)
@@ -39,11 +40,17 @@ class ProsperityMetrics(db.Model):
     TotalCapitalExpenditures = db.Column(db.Double)
     ShareBuyBacksAndDividendPayments = db.Column(db.Double)
 
-class GovernanceMetrics(db.Model):
+class Governancemetrics(db.Model):
     GovernanceMetricsID = db.Column(db.Integer, primary_key=True)
     AntiCorruptionTraining = db.Column(db.Double)
     ConfirmedCorruptionIncidentPrev = db.Column(db.Double)
     ConfirmedCorruptionIncidentCurrent = db.Column(db.Double)
+
+class Transaction(db.Model):
+    TransactionID = db.Column(db.Integer, primary_key=True)
+    ReportID = db.Column(db.Integer, db.ForeignKey('report.ReportID'))
+    NFTTransactionID = db.Column(db.String(100))
+    SubmissionTransactionID = db.Column(db.String(100))
 
 class Report(db.Model):
     ReportID = db.Column(db.Integer, primary_key=True)
@@ -54,12 +61,7 @@ class Report(db.Model):
     CreatedByID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
     CompanyID = db.Column(db.Integer, db.ForeignKey('company.CompanyID'))
     PeopleMetricsID = db.Column(db.Integer, db.ForeignKey('peoplemetrics.PeopleMetricsID'))
+    PlanetMetricsID = db.Column(db.Integer, db.ForeignKey('planetmetrics.PlanetMetricsID'))
     ProsperityMetricsID = db.Column(db.Integer, db.ForeignKey('prosperitymetrics.ProsperityMetricsID'))
     GovernanceMetricsID = db.Column(db.Integer, db.ForeignKey('governancemetrics.GovernanceMetricsID'))
     Status = db.Column(db.String(100))
-
-class Transaction(db.Model):
-    TransactionID = db.Column(db.Integer, primary_key=True)
-    ReportID = db.Column(db.Integer, db.ForeignKey('report.ReportID'))
-    NFTTransactionID = db.Column(db.String(100))
-    SubmissionTransactionID = db.Column(db.String(100))
